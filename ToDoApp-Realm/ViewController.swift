868c16193a8ed76def26f0d4481991a0dcb0b9bf
Mohamed Elbassiouny <apat4e@gmail.com>
2019-07-27 15:11:37 +0200

    init

---
+++ ToDoApp-Realm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp-Realm
+//
+//  Created by Mohamed Elbassiouny on 7/27/19.
+//  Copyright © 2019 Mohamed Elbassiouny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
